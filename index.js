90455a4fd3011f59f4bd917d2b74163a854290e7
TuMkoO <mr.t09@mail.ru>
2023-01-24 00:06:17 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const config = require("config");
+// const config = require("config");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const errorMiddleware = require("./middleware/error.middleware");
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: config.get("clientUrl"),
+    origin: process.env.CLIENT_URL,
   })
 );
 
@@ -40,11 +40,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get("mongoUri"), {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
