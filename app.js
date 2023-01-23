43ff73242bcc9a4c2550dcd070183b0ca24e7139
Timofey <mr.t09@mail.ru>
2023-01-23 14:40:44 +0300

    update node deploy

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
 const express = require("express");
-const cors = require("cors");
 const config = require("config");
-const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const errorMiddleware = require("./middleware/error.middleware");
 
 const app = express();
 app.use(express.json({ extended: true }));
-app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: config.get("clientUrl"),
-  })
-);
 
 app.use("/home", require("./routes/home"));
 app.use("/api/system", require("./routes/system.routes"));
-
-app.use(errorMiddleware);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + "/public/"));
-
-  app.get(/.*/, (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-  });
-}
 
 const PORT = config.get("port") || 5000;
 
