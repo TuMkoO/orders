68bb336c1e4747023d60802aeaed6a803e4ce068
Timofey <mr.t09@mail.ru>
2023-01-24 15:04:23 +0300

    update

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -40,12 +40,13 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
+        // browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
+        browser: ["es2015", "edge88", "firefox78", "chrome87", "safari13.1"],
         node: "node16",
       },
 
-      vueRouterMode: "hash", // available values: 'hash', 'history'
-      //vueRouterMode: "history", // available values: 'hash', 'history'
+      // vueRouterMode: "hash", // available values: 'hash', 'history'
+      vueRouterMode: "history", // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
